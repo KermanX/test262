035cc05070756bcc0eb6633186b6d6e3fde7cd4e
Ms2ger <Ms2ger@igalia.com>
2024-10-02 16:23:13 +0200

    wip

--- implementation-contributed/sm/non262/JSON/parse-with-source.js
+++ implementation-contributed/sm/non262/JSON/parse-with-source.js
@@ -113,37 +113,6 @@ esid: pending
     assert.sameValue(true, JSON.isRawJSON(JSON.rawJSON(1), JSON.rawJSON(2)));
 })();
 
-(function checkCrossCompartmentWrappers() {
-    if (!globalThis.isCCW) {
-        return;
-    }
-    var gbl = newGlobal({newCompartment: true});
-
-    // the created context object should be wrapped in this compartment
-    gbl.eval("context = JSON.parse('4.0', (k,v,context) => context);");
-    assert.sameValue(isCCW(gbl.context), true);
-    // an object created in the reviver function should be wrapped in this compartment
-    gbl.eval("sourceV = JSON.parse('4.0', (k,v,context) => { return {}; });");
-    assert.sameValue(isCCW(gbl.sourceV), true);
-
-    // objects created by a reviver in this compartment should be wrapped in
-    // the other compartment
-    gbl.rev = (k,v,c) => { return {v}};
-    gbl.eval("v2 = JSON.parse('4.0', rev);");
-    assert.sameValue(gbl.eval("isCCW(v2)"), true);
-
-    gbl.eval("objCCW = {};");
-    assert.sameValue(JSON.isRawJSON(gbl.objCCW), false, "isRawJSON() should accept CCW arguments");
-    rawJSONCCW = gbl.eval("JSON.rawJSON(455);");
-    assert.sameValue(JSON.isRawJSON(rawJSONCCW), true, "isRawJSON() should return true for wrapped rawJSON objects");
-
-    assert.sameValue(rawJSONCCW.rawJSON, "455", "rawJSON object enumerable property should be visible through CCW");
-
-    objWithCCW = { ccw: rawJSONCCW, raw: JSON.rawJSON(true) };
-    assert.sameValue(JSON.stringify(objWithCCW), '{"ccw":455,"raw":true}');
-    assert.sameValue(isCCW(rawJSONCCW), true);
-})();
-
 (function checkUseAsPrototype() {
     var p = JSON.rawJSON(false);
     var obj = { a: "hi" };
